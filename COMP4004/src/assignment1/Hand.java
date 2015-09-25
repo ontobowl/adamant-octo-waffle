436fe9bb789a10cf5cac8fb3eaad8aad1626c067
Graham Burry <cats_eye16@hotmail.com>
2015-09-24 22:05:06 -0400

    Implemented Hand class

---
+++ COMP4004/src/assignment1/Hand.java
@@ -0,0 +1,64 @@
+package assignment1;
+
+import assignment1.Card.Rank;
+import assignment1.Card.Suit;
+
+public class Hand {
+
+	public Boolean isComplete() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Boolean addCard(Rank r, Suit c) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setID(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addCard(Card c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getCard1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getCard2() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getCard3() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getCard4() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getCard5() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int compareTo(Hand h) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
