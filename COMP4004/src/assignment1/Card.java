6addb78ffd292f4e03b3f41b00c3b998135f6ac6
Graham Burry <cats_eye16@hotmail.com>
2015-09-24 22:55:38 -0400

    implemented equals() method

--- COMP4004/src/assignment1/Card.java
+++ COMP4004/src/assignment1/Card.java
@@ -36,42 +36,18 @@ public class Card {
 	//Getters
 	public Rank getRank() { return rank;	}
 	public Suit getSuit() { return suit; }
-//	private String rankToWord(int r) {
-//		String[] rankToWord = {
-//				"Two",
-//				"Three",
-//				"Four",
-//				"Five",
-//				"Six",
-//				"Seven",
-//				"Eight",
-//				"Nine",
-//				"Ten",
-//				"Jack",
-//				"Queen",
-//				"King",
-//				"Ace"
-//			};
-//		
-//		return rankToWord[r - 2];
-//	}
-//	
-//	private String suitToWord(Suit s) {
-//		switch(s) {
-//		case SPADES:
-//			return "Spades";
-//		case DIAMONDS:
-//			return "Diamonds";
-//		case CLUBS:
-//			return "Clubs";
-//		case HEARTS:
-//			return "Hearts";
-//		}
-//		return null;
-//	}
+	
+	
 	public String toString() {
 		String r = Character.toUpperCase(rank.toString().charAt(0)) + (rank.toString().substring(1)).toLowerCase();
 		String s = Character.toUpperCase(suit.toString().charAt(0)) + suit.toString().substring(1).toLowerCase();
 		return r + s;
+	}
+	
+	public Boolean equals(Card c) {
+		if(this.rank == c.getRank() && this.suit == c.getSuit())
+			return true;
+		else
+			return false;
 	}
 }
