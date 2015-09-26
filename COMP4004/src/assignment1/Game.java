b6b2c5b409e81a49796e3818217d65990d8a9753
Graham Burry <cats_eye16@hotmail.com>
2015-09-25 21:04:28 -0400

    created Game class

---
+++ COMP4004/src/assignment1/Game.java
@@ -0,0 +1,42 @@
+package assignment1;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class Game {
+
+	public Game(InputStream stream) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void queryNumPlayers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void queryPlayerHands() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Hand> getHands() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getNumPlayers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int[] getRanking() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getWinner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
