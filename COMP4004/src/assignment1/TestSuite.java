a0d6bc9444a3d176e1f0ec6a90b64cf3bc89a636
Graham Burry <cats_eye16@hotmail.com>
2015-09-28 01:44:02 -0400

    created a test suite

---
+++ COMP4004/src/assignment1/TestSuite.java
@@ -0,0 +1,15 @@
+package assignment1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  CardTest.class,
+  GameTest.class,
+  HandTest.class,
+})
+
+public class TestSuite {
+
+}
